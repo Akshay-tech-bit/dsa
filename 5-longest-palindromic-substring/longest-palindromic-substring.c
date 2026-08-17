6faab7929cb5be7e75f708acd266044aef541e87
Akshay pratap <akshaypratap34331@gmail.com>
2026-08-17 23:11:01 +0530

    Time: 8 ms (36.16%) | Memory: 9.1 MB (47.59%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.c
@@ -0,0 +1,47 @@
+#include <string.h>
+
+char* longestPalindrome(char* s) {
+    int n = strlen(s);
+
+    if (n < 2)
+        return s;
+
+    int start = 0;
+    int maxLen = 1;
+
+    for (int i = 0; i < n; i++) {
+
+        // Odd length palindrome
+        int left = i;
+        int right = i;
+
+        while (left >= 0 && right < n && s[left] == s[right]) {
+            if (right - left + 1 > maxLen) {
+                start = left;
+                maxLen = right - left + 1;
+            }
+
+            left--;
+            right++;
+        }
+
+        // Even length palindrome
+        left = i;
+        right = i + 1;
+
+        while (left >= 0 && right < n && s[left] == s[right]) {
+            if (right - left + 1 > maxLen) {
+                start = left;
+                maxLen = right - left + 1;
+            }
+
+            left--;
+            right++;
+        }
+    }
+
+    // Null terminate the answer
+    s[start + maxLen] = '\0';
+
+    return s + start;
+}
